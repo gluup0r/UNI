cd9ed6e8da4c4d52381b7338341559c62e5e518e
gluup0r <gluup0r13@gmail.com>
2024-10-15 09:25:32 +0300

    Add files via upload

---
+++ lr3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double m1 = 2;
+    double m2 = 4;
+    double m3 = 3;
+
+    double totalCost = m1 * 45.5 + m2 * 17.40 + m3 * 31.5;
+
+    cout << fixed << setprecision(2) << "Сума придбань = " << totalCost << " грн" << endl;
+}
